6e64b537a44bd1ec04ccca18fbf8fe06a490a0c8
Nimayn <nimayn2004@gmail.com>
2025-03-13 16:12:15 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, NIMAY\n";
-  cout << "Hello, CC LAB 08\n";
+  cout << "Hello, CC LAB \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
