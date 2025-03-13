a3e8b411bb7e39bdd2fdb46ea2c0096b94f60b25
Nimayn <nimayn2004@gmail.com>
2025-03-13 16:39:26 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, NIMAY\n";
+  cout << "Hello, CC LAB 08\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
